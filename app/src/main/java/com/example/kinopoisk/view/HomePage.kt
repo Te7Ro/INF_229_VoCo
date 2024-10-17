150c5e4ec3de88300401783659b8c6b77e07d2c6
nurtorequrmanqoja <nurtore.ad@gmail.com>
2024-10-18 00:42:15 +0500

    Design a composable for displaying each item in a LazyRow

--- app/src/main/java/com/example/kinopoisk/view/HomePage.kt
+++ app/src/main/java/com/example/kinopoisk/view/HomePage.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavController
 import com.example.kinopoisk.data.BottomNavigationItems
 import com.example.kinopoisk.model.BottomNavigationBar
 import com.example.kinopoisk.data.BottomNavGraph
+import org.w3c.dom.Text
 
 @Composable
 fun HomePage() {
@@ -31,3 +32,40 @@ val actionMoviesUSA = listOf(
     MovieItem(R.drawable.movie, "Близкие", "драма")
     MovieItem(R.drawable.movie, "Близкие", "драма")
 )
+
+
+@Composable
+fun MovieCard(item: MovieItem) {
+    Column {
+        Image(painterResource(item.image), contentDescription = null)
+        Text(text = item.title)
+    }
+}
+
+@Composable
+fun PremierMoviesSection() {
+    LazyRow {
+        items(premierMovies) {
+                item -> MovieCard(item)
+        }
+    }
+}
+@Composable
+fun PopularMoviesSection() {
+    LazyRow {
+        items(popularMovies) {
+                item -> MovieCard(item)
+        }
+    }
+}
+
+@Composable
+fun ActionMoviesUSA() {
+    LazyRow {
+        items(actionMoviesUSA) {
+                item -> MovieCard(item)
+        }
+    }
+}
+
+
