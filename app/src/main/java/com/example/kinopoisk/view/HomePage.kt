686e9763ae327f8b79bb8b4b1c8d75c2df0d797c
nurtorequrmanqoja <nurtore.ad@gmail.com>
2024-10-19 12:21:31 +0500

    added in home page fun

--- app/src/main/java/com/example/kinopoisk/view/HomePage.kt
+++ app/src/main/java/com/example/kinopoisk/view/HomePage.kt
@@ -10,62 +10,63 @@ import org.w3c.dom.Text
 
 @Composable
 fun HomePage() {
-}
+    data class MovieItem(val image: Int, val title: String, val genre: String)
 
-data class MovieItem(val image: Int, val title: String, val genre: String)
-
-val premierMovies = listOf(
+    val premierMovies = listOf(
+        MovieItem(R.drawable.movie, "Близкие", "драма")
+                MovieItem(R.drawable.movie, "Близкие", "драма")
     MovieItem(R.drawable.movie, "Близкие", "драма")
     MovieItem(R.drawable.movie, "Близкие", "драма")
+    )
+    val popularMovies = listOf(
+        MovieItem(R.drawable.movie, "Близкие", "драма")
+                MovieItem(R.drawable.movie, "Близкие", "драма")
     MovieItem(R.drawable.movie, "Близкие", "драма")
     MovieItem(R.drawable.movie, "Близкие", "драма")
-)
-val popularMovies = listOf(
+    )
+    val actionMoviesUSA = listOf(
+        MovieItem(R.drawable.movie, "Близкие", "драма")
+                MovieItem(R.drawable.movie, "Близкие", "драма")
     MovieItem(R.drawable.movie, "Близкие", "драма")
     MovieItem(R.drawable.movie, "Близкие", "драма")
-    MovieItem(R.drawable.movie, "Близкие", "драма")
-    MovieItem(R.drawable.movie, "Близкие", "драма")
-)
-val actionMoviesUSA = listOf(
-    MovieItem(R.drawable.movie, "Близкие", "драма")
-    MovieItem(R.drawable.movie, "Близкие", "драма")
-    MovieItem(R.drawable.movie, "Близкие", "драма")
-    MovieItem(R.drawable.movie, "Близкие", "драма")
-)
+    )
 
 
-@Composable
-fun MovieCard(item: MovieItem) {
-    Column {
-        Image(painterResource(item.image), contentDescription = null)
-        Text(text = item.title)
-    }
-}
-
-@Composable
-fun PremierMoviesSection() {
-    LazyRow {
-        items(premierMovies) {
-                item -> MovieCard(item)
+    @Composable
+    fun MovieCard(item: MovieItem) {
+        Column {
+            Image(painterResource(item.image), contentDescription = null)
+            Text(text = item.title)
         }
     }
-}
-@Composable
-fun PopularMoviesSection() {
-    LazyRow {
-        items(popularMovies) {
-                item -> MovieCard(item)
+
+    @Composable
+    fun PremierMoviesSection() {
+        LazyRow {
+            items(premierMovies) {
+                    item -> MovieCard(item)
+            }
         }
     }
+    @Composable
+    fun PopularMoviesSection() {
+        LazyRow {
+            items(popularMovies) {
+                    item -> MovieCard(item)
+            }
+        }
+    }
+
+    @Composable
+    fun ActionMoviesUSA() {
+        LazyRow {
+            items(actionMoviesUSA) {
+                    item -> MovieCard(item)
+            }
+        }
+    }
+
 }
 
-@Composable
-fun ActionMoviesUSA() {
-    LazyRow {
-        items(actionMoviesUSA) {
-                item -> MovieCard(item)
-        }
-    }
-}
 
 
