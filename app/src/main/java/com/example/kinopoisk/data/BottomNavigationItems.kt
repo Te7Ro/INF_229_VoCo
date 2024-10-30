4a34aaecd2fdf65f2f42812ce7856ff1ed4a1682
Aizharykov <119413170+Aizharykov@users.noreply.github.com>
2024-10-30 20:19:17 +0500

    changes after defense 2

--- app/src/main/java/com/example/kinopoisk/data/BottomNavigationItems.kt
+++ app/src/main/java/com/example/kinopoisk/data/BottomNavigationItems.kt
@@ -7,15 +7,15 @@ sealed class BottomNavigationItems (
     val route: String,
     val icon: Int,
 ){
-    object Home : BottomNavigationItems(
+    data object Home : BottomNavigationItems(
         route = "home",
         icon = R.drawable.home_icon
     )
-    object Search : BottomNavigationItems(
+    data object Search : BottomNavigationItems(
         route = "search",
         icon = R.drawable.search_icon
     )
-    object Profile : BottomNavigationItems(
+    data object Profile : BottomNavigationItems(
         route = "profile",
         icon = R.drawable.profile_icon
     )
